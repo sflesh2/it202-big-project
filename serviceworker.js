3e5d3419d7d4880290d704617e7e614fe89b13d7
Shawn Flesher <sflesh2@uic.edu>
2020-05-03 07:46:09 +0000

    these files were never added... whoops

---
+++ serviceworker.js
@@ -0,0 +1,28 @@
+const chicagoCrime = "dev-chiCrime-site-v3"
+const assets = [
+  "/",
+  "/index.html",
+  "/css/format.css",
+  "/js/info.js",
+  "/favicon.ico",
+  "https://unpkg.com/material-components-web@v4.0.0/dist/material-components-web.min.css",
+  "https://unpkg.com/material-components-web@v4.0.0/dist/material-components-web.min.js",
+  "https://fonts.googleapis.com/icon?family=Material+Icons",
+  "https://unpkg.com/dexie@latest/dist/dexie.js"
+]
+
+self.addEventListener("install", installEvent => {
+  installEvent.waitUntil(
+    caches.open(chicagoCrime).then(cache => {
+      cache.addAll(assets)
+    })
+  )
+})
+
+self.addEventListener("fetch", fetchEvent => {
+  fetchEvent.respondWith(
+    caches.match(fetchEvent.request).then(res => {
+      return res || fetch(fetchEvent.request)
+    })
+  )
+})
