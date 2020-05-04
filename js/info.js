973e2efc068e93a2a2ae82274824188a249c4495
Shawn Flesher <sflesh2@uic.edu>
2020-05-04 03:50:43 +0000

    file isn't working with github pages?

--- js/info.js
+++ js/info.js
@@ -449,7 +449,7 @@ let loadWeather = ()=>{
 if ("serviceWorker" in navigator) {
   window.addEventListener("load", function() {
     navigator.serviceWorker
-      .register("./serviceworker.js")
+      .register("/serviceworker.js")
       .then(res => console.log("service worker registered"))
       .catch(err => console.log("service worker not registered", err))
   })
