301f2a93bf636ab09472fb7e7257c4958b709ff5
Shawn Flesher <sflesh2@uic.edu>
2020-05-04 04:02:40 +0000

    this wasn't working

--- js/info.js
+++ js/info.js
@@ -449,7 +449,7 @@ let loadWeather = ()=>{
 if ("serviceWorker" in navigator) {
   window.addEventListener("load", function() {
     navigator.serviceWorker
-      .register("../serviceworker.js")
+      .register("https://sflesh2.github.io/serviceworker.js")
       .then(res => console.log("service worker registered"))
       .catch(err => console.log("service worker not registered", err))
   })
